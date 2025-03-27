f3b5c0d1760f254fd356a01b4585da10450e97ea
tom-new <tomnewtonnew@gmail.com>
2025-03-27 12:39:57 +1100

    linting compliance

--- gdrift/anelasticity.py
+++ gdrift/anelasticity.py
@@ -7,7 +7,10 @@ from .mineralogy import ThermodynamicModel
 from typing import Type, TypeVar, Callable
 import numpy.typing as npt
 
-AnelasticityModel = TypeVar("AnelasticityModel", bound="BaseAnelasticityModel") # define a type hint for any subclass of `BaseAnelasticityModel`
+AnelasticityModel = TypeVar(
+    "AnelasticityModel", bound="BaseAnelasticityModel"
+)  # define a type hint for any subclass of `BaseAnelasticityModel`
+
 
 class BaseAnelasticityModel(ABC):
     """
@@ -46,8 +49,8 @@ class BaseAnelasticityModel(ABC):
     def build_ghelichkhan_solidus():
         # Load the solidus curve for the mantle from Andrault et al. (2011)
         andrault_solidus = RadialEarthModelFromFile(
-            model_name="1d_solidus_Andrault_et_al_2011_EPSL",
-            description="Andrault et al. 2011, EPSL")
+            model_name="1d_solidus_Andrault_et_al_2011_EPSL", description="Andrault et al. 2011, EPSL"
+        )
 
         # Load the solidus curve for the mantle from Hirschmann (2000)
         hirsch_solidus = HirschmannSolidus()
@@ -55,7 +58,7 @@ class BaseAnelasticityModel(ABC):
         # Combining the two
         my_depths = []
         my_solidus = []
-        for solidus_model in [hirsch_solidus, andrault_solidus.get_profile('solidus temperature')]:
+        for solidus_model in [hirsch_solidus, andrault_solidus.get_profile("solidus temperature")]:
             d_min, d_max = solidus_model.min_max_depth()
             dpths = numpy.arange(d_min, d_max, 10e3)
             my_depths.extend(dpths)
@@ -70,7 +73,8 @@ class BaseAnelasticityModel(ABC):
             depth=numpy.asarray(my_depths),
             value=numpy.asarray(my_solidus),
             extrapolate=True,
-            name="Ghelichkhan et al. 2021")
+            name="Ghelichkhan et al. 2021",
+        )
 
         return ghelichkhan_et_al
 
@@ -80,13 +84,15 @@ class CammaranoAnelasticityModel(BaseAnelasticityModel):
     A specific implementation of an anelasticity model following the approach by Cammarano et al. (2003).
     """
 
-    def __init__(self,
-                 B: Callable[[npt.ArrayLike], npt.NDArray],
-                 g: Callable[[npt.ArrayLike], npt.NDArray],
-                 a: Callable[[npt.ArrayLike], npt.NDArray],
-                 omega: Callable[[npt.ArrayLike], npt.NDArray],
-                 solidus: Type[SplineProfile],
-                 Q_bulk: Callable[[npt.ArrayLike], npt.NDArray]):
+    def __init__(
+        self,
+        B: Callable[[npt.ArrayLike], npt.NDArray],
+        g: Callable[[npt.ArrayLike], npt.NDArray],
+        a: Callable[[npt.ArrayLike], npt.NDArray],
+        omega: Callable[[npt.ArrayLike], npt.NDArray],
+        solidus: Type[SplineProfile],
+        Q_bulk: Callable[[npt.ArrayLike], npt.NDArray],
+    ):
         """
         Initialize the model with the given parameters.
 
@@ -114,42 +120,34 @@ class CammaranoAnelasticityModel(BaseAnelasticityModel):
             q_profile (str): The name of the parameter set to use (e.g. "Q1" for Q1).
         """
         parameters = {
-            "Q1": {
-                "B": [0.5, 10],
-                "g": [20, 10]
-            },
-            "Q2": {
-                "B": [0.8, 15],
-                "g": [20, 10]
-            },
-            "Q3": {
-                "B": [1.1, 20],
-                "g": [20, 10]
-            },
-            "Q4": {
-                "B": [0.035, 2.25],
-                "g": [30, 15]
-            },
-            "Q5": {
-                "B": [0.056, 3.6],
-                "g": [30, 15]
-            },
-            "Q6": {
-                "B": [0.077, 4.95],
-                "g": [30, 15]
-            }
+            "Q1": {"B": [0.5, 10], "g": [20, 10]},
+            "Q2": {"B": [0.8, 15], "g": [20, 10]},
+            "Q3": {"B": [1.1, 20], "g": [20, 10]},
+            "Q4": {"B": [0.035, 2.25], "g": [30, 15]},
+            "Q5": {"B": [0.056, 3.6], "g": [30, 15]},
+            "Q6": {"B": [0.077, 4.95], "g": [30, 15]},
         }
 
         if q_profile not in parameters.keys():
             raise ValueError(f"Invalid argument: {q_profile}. Must be one of {parameters.keys()}")
 
-        B = lambda x: numpy.where(x < 660e3, parameters[q_profile]["B"][0], parameters[q_profile]["B"][1])
-        g = lambda x: numpy.where(x < 660e3, parameters[q_profile]["g"][0], parameters[q_profile]["g"][1])
-        a = lambda x: 0.2 * numpy.ones_like(x)
-        omega = lambda x: numpy.ones_like(x)
+        def B(x):
+            return numpy.where(x < 660e3, parameters[q_profile]["B"][0], parameters[q_profile]["B"][1])
+
+        def g(x):
+            return numpy.where(x < 660e3, parameters[q_profile]["g"][0], parameters[q_profile]["g"][1])
+
+        def a(x):
+            return 0.2 * numpy.ones_like(x)
+
+        def omega(x):
+            return numpy.ones_like(x)
+
+        def Q_bulk(x):
+            return numpy.where(x < 660e3, 1e3, 1e4)
+
         solidus = cls.build_ghelichkhan_solidus()
-        Q_bulk = lambda x: numpy.where(x < 660e3, 1e3, 1e4)
-        
+
         return cls(B=B, g=g, a=a, omega=omega, solidus=solidus, Q_bulk=Q_bulk)
 
     def compute_Q_shear(self, depths: npt.ArrayLike, temperatures: npt.ArrayLike) -> npt.NDArray:
@@ -179,8 +177,9 @@ class CammaranoAnelasticityModel(BaseAnelasticityModel):
         temperatures = numpy.asarray(temperatures)
 
         Q_values = (
-            self.B(depths) * (self.omega(depths)**self.a(depths)) * numpy.exp(
-                (self.a(depths) * self.g(depths) * self.solidus.at_depth(depths)) / temperatures)
+            self.B(depths)
+            * (self.omega(depths) ** self.a(depths))
+            * numpy.exp((self.a(depths) * self.g(depths) * self.solidus.at_depth(depths)) / temperatures)
         )
 
         return Q_values
@@ -203,19 +202,22 @@ class CammaranoAnelasticityModel(BaseAnelasticityModel):
         temperatures = numpy.asarray(temperatures)
 
         return self.Q_bulk(depths)
-    
+
+
 class GoesAnelasticityModel(BaseAnelasticityModel):
     """
     A specific implementation of an anelasticity model following the approach by Goes et al. (2004).
     """
 
-    def __init__(self,
-                 Q0: Callable[[npt.ArrayLike], npt.NDArray],
-                 xi: Callable[[npt.ArrayLike], npt.NDArray],
-                 a: Callable[[npt.ArrayLike], npt.NDArray],
-                 omega: Callable[[npt.ArrayLike], npt.NDArray],
-                 solidus: Type[SplineProfile],
-                 Q_bulk: Callable[[npt.ArrayLike], npt.NDArray]):
+    def __init__(
+        self,
+        Q0: Callable[[npt.ArrayLike], npt.NDArray],
+        xi: Callable[[npt.ArrayLike], npt.NDArray],
+        a: Callable[[npt.ArrayLike], npt.NDArray],
+        omega: Callable[[npt.ArrayLike], npt.NDArray],
+        solidus: Type[SplineProfile],
+        Q_bulk: Callable[[npt.ArrayLike], npt.NDArray],
+    ):
         """
         Initialize the model with the given parameters.
 
@@ -242,27 +244,28 @@ class GoesAnelasticityModel(BaseAnelasticityModel):
         Args:
             q_profile (str): The name of the parameter set to use (e.g. "Q4" for Q4).
         """
-        parameters = {
-            "Q4": {
-                "Q0": [3.5, 35],
-                "xi": [20, 10]
-            },
-            "Q6": {
-                "Q0": [0.5, 3.5],
-                "xi": [30, 20]
-            }
-        }
+        parameters = {"Q4": {"Q0": [3.5, 35], "xi": [20, 10]}, "Q6": {"Q0": [0.5, 3.5], "xi": [30, 20]}}
 
         if q_profile not in parameters.keys():
             raise ValueError(f"Invalid argument: {q_profile}. Must be one of {parameters.keys()}")
 
-        Q0 = lambda x: numpy.where(x < 660e3, parameters[q_profile]["Q0"][0], parameters[q_profile]["Q0"][1])
-        xi = lambda x: numpy.where(x < 660e3, parameters[q_profile]["xi"][0], parameters[q_profile]["xi"][1])
-        a = lambda x: 0.15 * numpy.ones_like(x)
-        omega = lambda x: numpy.ones_like(x)
+        def Q0(x):
+            return numpy.where(x < 660e3, parameters[q_profile]["Q0"][0], parameters[q_profile]["Q0"][1])
+
+        def xi(x):
+            return numpy.where(x < 660e3, parameters[q_profile]["xi"][0], parameters[q_profile]["xi"][1])
+
+        def a(x):
+            return 0.15 * numpy.ones_like(x)
+
+        def omega(x):
+            return numpy.ones_like(x)
+
+        def Q_bulk(x):
+            return numpy.where(x < 660e3, 1e3, 1e4)
+
         solidus = cls.build_ghelichkhan_solidus()
-        Q_bulk = lambda x: numpy.where(x < 660e3, 1e3, 1e4)
-        
+
         return cls(Q0=Q0, xi=xi, a=a, omega=omega, solidus=solidus, Q_bulk=Q_bulk)
 
     def compute_Q_shear(self, depths: npt.ArrayLike, temperatures: npt.ArrayLike) -> npt.NDArray:
@@ -292,8 +295,9 @@ class GoesAnelasticityModel(BaseAnelasticityModel):
         temperatures = numpy.asarray(temperatures)
 
         Q_values = (
-            self.Q0(depths) * (self.omega(depths)**self.a(depths)) * numpy.exp(
-                (self.a(depths) * self.xi(depths) * self.solidus.at_depth(depths)) / temperatures)
+            self.Q0(depths)
+            * (self.omega(depths) ** self.a(depths))
+            * numpy.exp((self.a(depths) * self.xi(depths) * self.solidus.at_depth(depths)) / temperatures)
         )
 
         return Q_values
@@ -317,6 +321,7 @@ class GoesAnelasticityModel(BaseAnelasticityModel):
 
         return self.Q_bulk(depths)
 
+
 def apply_anelastic_correction(thermo_model: Type[ThermodynamicModel], anelastic_model: Type[AnelasticityModel]):
     """
     Apply anelastic corrections to seismic velocity data using the provided "anelastic_model"
@@ -325,7 +330,7 @@ def apply_anelastic_correction(thermo_model: Type[ThermodynamicModel], anelastic
     as described by Stixrude & Lithgow-Bertelloni (doi:10.1029/2004JB002965, Eq-10).
 
         thermo_model (Type[ThermodynamicModel]): The thermodynamic model containing temperature and depth data.
-        anelastic_model (Type[AnelasticityModel]): The anelasticity model containing 
+        anelastic_model (Type[AnelasticityModel]): The anelasticity model containing
 
     Returns:
         Type[ThermodynamicModel]: A new thermodynamic model with anelastically corrected seismic velocities.
@@ -343,6 +348,7 @@ def apply_anelastic_correction(thermo_model: Type[ThermodynamicModel], anelastic
     quality factor matrices for shear and bulk moduli. The corrected seismic velocities are then calculated and returned
     as new tables with the corrected values.
     """
+
     class ThermodynamicModelPrime(thermo_model.__class__):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
@@ -365,24 +371,22 @@ def apply_anelastic_correction(thermo_model: Type[ThermodynamicModel], anelastic
             #
             swave_speed_table = super().compute_swave_speed()
             depths_x, temperatures_x = numpy.meshgrid(
-                swave_speed_table.get_x(),
-                swave_speed_table.get_y(),
-                indexing="ij")
+                swave_speed_table.get_x(), swave_speed_table.get_y(), indexing="ij"
+            )
 
             # For shear wave velocity Q = Q_\mu
-            Q_matrix = anelastic_model.compute_Q_shear(
-                depths_x, temperatures_x)
+            Q_matrix = anelastic_model.compute_Q_shear(depths_x, temperatures_x)
 
             # Anelastically corrected shear wave values
-            corrected_vals = (
-                swave_speed_table.get_vals() * (1 - 0.5 / numpy.tan(anelastic_model.a(depths_x) * numpy.pi / 2) / Q_matrix)
+            corrected_vals = swave_speed_table.get_vals() * (
+                1 - 0.5 / numpy.tan(anelastic_model.a(depths_x) * numpy.pi / 2) / Q_matrix
             )
             #
             return type(swave_speed_table)(
                 x=swave_speed_table.get_x(),
                 y=swave_speed_table.get_y(),
                 vals=corrected_vals,
-                name=f"{swave_speed_table.get_name()}"
+                name=f"{swave_speed_table.get_name()}",
             )
 
         def compute_pwave_speed(self):
@@ -403,7 +407,7 @@ def apply_anelastic_correction(thermo_model: Type[ThermodynamicModel], anelastic
 
             Returns:
                 Table: Anelastically corrected compressional wave speed table.
-            """            # compute s and p wave velocities to compute "L".
+            """  # compute s and p wave velocities to compute "L".
             pwave_speed_table = super().compute_pwave_speed()
             swave_speed_table = super().compute_swave_speed()
 
@@ -412,17 +416,17 @@ def apply_anelastic_correction(thermo_model: Type[ThermodynamicModel], anelastic
 
             # Meshing depths and temperatures of the anharmonic model to get all combinations
             depths_x, temperatures_x = numpy.meshgrid(
-                pwave_speed_table.get_x(), pwave_speed_table.get_y(), indexing="ij")
-
-            # computing Q_matrix for compressional wave
-            Q_matrix_inv = (
-                L / anelastic_model.compute_Q_shear(depths_x, temperatures_x) + (
-                    1 - L) / anelastic_model.compute_Q_bulk(depths_x, temperatures_x)
+                pwave_speed_table.get_x(), pwave_speed_table.get_y(), indexing="ij"
             )
 
+            # computing Q_matrix for compressional wave
+            Q_matrix_inv = L / anelastic_model.compute_Q_shear(depths_x, temperatures_x) + (
+                1 - L
+            ) / anelastic_model.compute_Q_bulk(depths_x, temperatures_x)
+
             # Apply anelastic correction
-            corrected_vals = (
-                pwave_speed_table.get_vals() * (1 - 0.5 / numpy.tan(anelastic_model.a(depths_x) * numpy.pi / 2) * Q_matrix_inv)
+            corrected_vals = pwave_speed_table.get_vals() * (
+                1 - 0.5 / numpy.tan(anelastic_model.a(depths_x) * numpy.pi / 2) * Q_matrix_inv
             )
 
             # return the anelastically corrected table
@@ -430,12 +434,9 @@ def apply_anelastic_correction(thermo_model: Type[ThermodynamicModel], anelastic
                 x=pwave_speed_table.get_x(),
                 y=pwave_speed_table.get_y(),
                 vals=corrected_vals,
-                name=f"{pwave_speed_table.get_name()}"
+                name=f"{pwave_speed_table.get_name()}",
             )
 
     return ThermodynamicModelPrime(
-        thermo_model.model,
-        thermo_model.composition,
-        thermo_model.get_temperatures(),
-        thermo_model.get_depths()
+        thermo_model.model, thermo_model.composition, thermo_model.get_temperatures(), thermo_model.get_depths()
     )
